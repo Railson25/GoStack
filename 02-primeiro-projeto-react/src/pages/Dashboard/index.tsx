87ad3f7b2392a40f351fbb264ab51cc90f36f855
Railson Santiago <railsonsantiago123@gmail.com>
2021-03-09 12:54:22 -0300

    GoStack

---
+++ 02-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,78 @@
+import React, {FormEvent, useState} from 'react'
+import { FiChevronRight } from 'react-icons/fi' 
+import api from '../../services/api'
+
+import logoImg from '../../assets/logo.svg'
+import { Title, Form, Repositories, Error} from  './styles'
+
+interface Repository {
+    full_name: string
+    description: string
+    owner: {
+        login: string
+        avatar_url: string
+    }
+}
+
+const Dashboard: React.FC = () => {
+    const [newRepository, setNewRepository] = useState('')
+    const [inputError, setInputError] = useState('')
+    const [ repositories, setRepositories] = useState<Repository[]>([])
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault()
+        if(!newRepository) {
+            setInputError('Digite o autor/nome do Repositório')
+            return
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepository}`)
+            const repository = response.data
+    
+            setRepositories([...repositories, repository])
+            setNewRepository('') 
+            setInputError('')
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório')
+        }
+        
+
+    }
+
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer"/>
+            <Title>Explore repositórios no Github</Title>
+
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input 
+                    value={newRepository}
+                    onChange={(e) => setNewRepository(e.target.value)}
+                    placeholder='Digite o nome do repositório '
+                />
+                <button type='submit'>Pesquisar</button>
+            </Form>
+
+            {inputError && <Error>{inputError}</Error>}
+
+            <Repositories>
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
+                        <img 
+                            src={repository.owner.avatar_url} 
+                            alt={repository.owner.login} 
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
+            </Repositories>
+        </>
+    )
+}
+
+export default Dashboard
