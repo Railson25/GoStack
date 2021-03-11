b2c3a981ab568ea49cafd7da59c28d37454bca8c
Railson Santiago <railsonsantiago123@gmail.com>
2021-03-10 21:59:42 -0300

    Componente repository

--- 02-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 02-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
-import React, {FormEvent, useState} from 'react'
-import { FiChevronRight } from 'react-icons/fi' 
-import api from '../../services/api'
+import React, {FormEvent, useEffect, useState} from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
+import api from '../../services/api'
 import logoImg from '../../assets/logo.svg'
 import { Title, Form, Repositories, Error} from  './styles'
 
@@ -17,7 +18,20 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepository, setNewRepository] = useState('')
     const [inputError, setInputError] = useState('')
-    const [ repositories, setRepositories] = useState<Repository[]>([])
+    const [ repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+        if(storagedRepositories) {
+            return JSON.parse(storagedRepositories)
+        } else {
+            return []
+        }
+    })
+
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault()
@@ -58,7 +72,7 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
                         <img 
                             src={repository.owner.avatar_url} 
                             alt={repository.owner.login} 
@@ -68,7 +82,7 @@ const Dashboard: React.FC = () => {
                             <p>{repository.description}</p>
                         </div>
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- 02-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ 02-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,63 @@
 import React from 'react'
+import {useRouteMatch, Link} from 'react-router-dom'
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+import {Header, RepositoryInfo, Issues} from './styles'
+
+interface RepositoryParams {
+    repository: string
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>
+    const { params } = useRouteMatch<RepositoryParams>()
+
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer"/>
+                <Link to='/'>
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars.githubusercontent.com/u/47870113?s=460&u=02b73fc747977be6c65fdfc9799f55c45d8eae5d&v=4" 
+                        alt="Railson Santiago"/>
+                    <div>
+                        <strong>Railson25/GoStack</strong>
+                        <p>Descrição do repositório</p>
+                    </div>    
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link  to='DERFH'>
+                    <div>
+                        <strong>dfgh</strong>
+                        <p>fdgjdfg</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    )
 }
 
 export default Repository
