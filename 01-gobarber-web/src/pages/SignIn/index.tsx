697605766872b21843d7921ff26e06c3119c4cf4
Railson Santiago <railsonsantiago123@gmail.com>
2021-06-27 12:26:40 -0300

    Autenticaçao && Mensagens Toast

--- 01-gobarber-web/src/pages/SignIn/index.tsx
+++ 01-gobarber-web/src/pages/SignIn/index.tsx
@@ -4,6 +4,8 @@ import { FormHandles } from "@unform/core";
 import {Form} from '@unform/web'
 import * as Yup from 'yup'
 
+import {useAuth} from '../../hooks/auth'
+import {useToast} from '../../hooks/toast';
 import getValidationErrors from "../../utils/getValidationErros";
 
 import logoImg from '../../assets/logo.svg'
@@ -12,10 +14,18 @@ import {Container, Content, Background} from './styles'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
+interface SignInFormData {
+    email: string
+    password: string
+}
+
 const SignIn: React.FC  = () => {
     const formRef = useRef<FormHandles>(null)
+
+    const {signIn } = useAuth()
+    const { addToast } = useToast()
     
-    const handleSubmit = useCallback(async (data: object) => {
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         try {
             formRef.current?.setErrors({})
             const schema = Yup.object().shape({
@@ -25,13 +35,24 @@ const SignIn: React.FC  = () => {
             await schema.validate(data, {
                 abortEarly: false
             })
+            await signIn({
+                email: data.email,
+                password: data.password
+            })
         } catch(err) {
-            const errors = getValidationErrors(err)
-
-            formRef.current?.setErrors(errors)
+            if (err instanceof Yup.ValidationError){
+                const errors = getValidationErrors(err)
+    
+                formRef.current?.setErrors(errors)
+            }
             
+            addToast({
+                type: 'info',
+                title: 'Erro na autenticação',
+                description: 'Ocorreu um erro ao fazer Login, cheque as credenciais',
+            })
         }
-    }, [])
+    }, [signIn, addToast])
 
     return (
         <Container>
