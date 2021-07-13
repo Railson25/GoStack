11c889418c944b96ec3ce24a73bae9709cfdfca4
Railson Santiago <railsonsantiago123@gmail.com>
2021-07-12 21:29:14 -0300

    Aplicando conceitos SOLID

--- primeiro-projeto-node/src/modules/users/services/CreateUserService.ts
+++ primeiro-projeto-node/src/modules/users/services/CreateUserService.ts
@@ -1,35 +1,35 @@
-import { getRepository } from 'typeorm'
 import { hash } from 'bcryptjs'
 
 import AppError from '@shared/errors/AppError'
 import User from '../infra/typeorm/entities/User'
+import IUsersRepository from '../repositories/IUsersRepository'
 
-interface Request {
+interface IRequest {
     name: String,
     email: String,
     password: String
 }
 
 class CreateUserService {
-    public async execute({name, email, password}: Request): Promise<User> {
-        const usersRepository = getRepository(User)
+  constructor(private usersRepository: IUsersRepository){}
 
-        const checkUserExists = await usersRepository.findOne({
+    public async execute({name, email, password}: IRequest): Promise<User> {
+
+        const checkUserExists = await this.usersRepository.findByEmail({
             where: {email},
         })
-        
+
         if (checkUserExists) {
             throw new AppError('Email address already used. ')
         }
 
         const hashedPassword = await hash(password, 8)
 
-        const user = usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
-            email, 
+            email,
             password: hashedPassword
         })
-        await usersRepository.save(user)
         return user
     }
 }
--- primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.ts
+++ primeiro-projeto-node/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,41 +1,41 @@
 import path from 'path'
-import { getRepository } from 'typeorm'
 import fs from 'fs'
 
+import IUsersRepository from '../repositories/IUsersRepository'
 import AppError from '@shared/errors/AppError'
 import UploadConfig from '@config/upload'
 import User from '../infra/typeorm/entities/User'
 
-interface Request {
+interface IRequest {
     user_id:string
     avatarFilename: string
 }
 
 class UpdtateUserAvatarService {
-    public async execute({user_id, avatarFilename }: Request): Promise<User> {
-        const usersRepository = getRepository(User)
+  constructor(private usersRepository: IUsersRepository){}
+    public async execute({user_id, avatarFilename }: IRequest): Promise<User> {
 
-        const user = await usersRepository.findOne(user_id)
+        const user = await this.usersRepository.findById(user_id)
 
         if(!user) {
             throw new AppError('Only authenticated users can change avatar. ', 401)
         }
         if(user.avatar) {
             const userAvatarFilePath = path.join(UploadConfig.directory, user.avatar)
-            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)    
-            
+            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
+
             if(userAvatarFileExists) {
             await fs.promises.unlink(userAvatarFilePath)
-            }    
+            }
         }
 
         user.avatar = avatarFilename
 
-        await usersRepository.save(user)
+        await this.usersRepository.save(user)
 
         return user
 
     }
 }
 
-export default UpdtateUserAvatarService 
+export default UpdtateUserAvatarService
