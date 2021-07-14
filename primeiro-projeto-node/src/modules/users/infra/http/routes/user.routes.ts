d21b67cc485132fd61cbdfdb68405de974a0dbe8
Railson Santiago <railsonsantiago123@gmail.com>
2021-07-13 21:34:00 -0300

    Injeçao de Dependencias && Controllers

--- primeiro-projeto-node/src/modules/users/infra/http/routes/user.routes.ts
+++ primeiro-projeto-node/src/modules/users/infra/http/routes/user.routes.ts
@@ -1,47 +1,19 @@
 import multer from 'multer'
-import { response, Router} from 'express'
+import { Router} from 'express'
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
-import CreateUserService from '@modules/users/services/CreateUserService'
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
+import UsersController from '../controllers/UsersController'
+import UserAvatarController from '../controllers/UserAvatarController'
 import uploadConfig from '@config/upload'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
 const upload = multer(uploadConfig)
+const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
-usersRouter.post('/', async (request, response) => {
-        const {name , email, password } = request.body
+usersRouter.post('/', usersController.create)
 
-        const usersRepository = new UsersRepository()
-        const createUser = new CreateUserService(usersRepository)
-
-        const user = await createUser.execute({
-            name,
-            email,
-            password
-        })
-
-        delete user.password
-
-      return response.json(user)
-})
-
-usersRouter.patch('/avatar',ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-
-        const usersRepository = new UsersRepository()
-        const updateUserAvatar = new UpdateUserAvatarService(usersRepository)
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename
-        })
-
-        delete user.password
-
-        return response.json(user)
-
-})
+usersRouter.patch('/avatar',ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 export default usersRouter
 
